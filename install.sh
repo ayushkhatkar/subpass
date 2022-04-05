334f1d8f0aa9feba36e0cc52c280de5be24aa66c
ayush khatkar <91492885+ayush0x01@users.noreply.github.com>
2022-04-05 19:38:18 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+echo "subpass installing requirment";
+
+chmod +x sub.sh;
+chmod +x passterbo.sh;
+
+alias sub='./passterbo.sh' >> .bashrc;
+source .bashrc;
+echo "all set good luck";
+echo "sub [target.com]";
